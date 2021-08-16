cc0e0cb5206f260d1e5a504bdc17a6b3e3ae7ebf
ChristianWitzler <57713653+ChristianWitzler@users.noreply.github.com>
2021-08-16 09:50:36 +0200

    Update utility.py
    More pythonic Function

--- pdi2sensei/utility.py
+++ pdi2sensei/utility.py
@@ -2,9 +2,16 @@ import vtk
 
 
 
-def getVtkImageData(nx, ny, nz=1, ox=0, oy=0 ,oz=0, sx=1, sy=1, sz=1, ghostLvl=0):
+def getVtkImageData(size, origin = [0,0,0], spacing=[1,1,1], ghostLvl=0):
+  """Function building a VTKImageData as a mesh, accepting an array for size, origin and spacing
+  All three arrays can contain 2 or three elements, either for a 2D or 3D case"""
   grid = vtk.vtkImageData()
-  grid.SetDimensions(nx,ny,nz)
-  grid.SetSpacing(sx,sy,sz)
-  grid.SetOrigin(ox,oy,oz)
+  # in case of a two dimensional array
+  if len(size) == 2:
+    size = [size[0], size[1], 1]
+    origin = [origin[0], origin[1], 0]
+    spacing = [spacing[0], spacing[1], 1]
+  grid.SetDimensions(size[0],size[1],size[2])
+  grid.SetSpacing(spacing[0],spacing[1],spacing[2])
+  grid.SetOrigin(origin[0],origin[1],origin[2])
   return grid
